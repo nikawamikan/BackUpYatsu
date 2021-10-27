b45afa1a46f4e3680fa088163d360e88f140059f
nikawamikan <wkwktktkkrkr@outlook.jp>
2021-10-27 13:10:42 +0900

    commit

--- src/main/java/com/example/Necessary.java
+++ src/main/java/com/example/Necessary.java
@@ -5,13 +5,28 @@ import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Gsonが認識するために必要なクラスです。<br/>
+ * 形式等名前が一致したJsonオブジェクトをこのクラスに変換する
+ * 
+ * @param nece   コピーするファイルを指定するためのリストです。
+ * @param except 例外的に不要なファイルを指定するリストです。
+ * 
+ * @author nikawamikan
+ */
 public class Necessary {
 
     @SerializedName("nece")
     @Expose
+    /**
+     * コピーするファイルを指定するためのリスト
+     */
     private List<String> nece = null;
     @SerializedName("except")
     @Expose
+    /**
+     * 例外的に不要なファイルを指定するリスト
+     */
     private List<String> except = null;
 
     public List<String> getNece() {
